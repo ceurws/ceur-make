95e7bd72a61c5cfc75c0cd4fc75b4fa9b685172d
Christoph Lange <ch.lange@jacobs-university.de>
2012-10-17 14:40:47 +0100

    link to project

---
+++ easychair2xml.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl -wln
+#
+# Script to generate one <paper> element in toc.xml, out of the information in the respective VVVVPPPP/README_EASYCHAIR file
+# Not to be invoked standalone, but from “make toc.xml”
+#
+# Part of ceur-make (https://github.com/clange/ceur-make/)
+#
+# © Christoph Lange 2012
+#
+# Licensed under GPLv3 or any later version
+
+BEGIN {
+    $authors = -1;
+    print "    <paper>";
+}
+
+print "        <pages from=\"$1\" to=\"$2\"/>" if /^Pages: (\d+)-(\d+)$/;
+print "        <title>$1</title>" if /^Paper: (.*)$/;
+if (/^-+$/ && $authors == -1) {
+    $authors = 0;
+}
+if (/^$/ && $authors == 0) {
+    $authors = 1;
+    print "        <authors>";
+}
+print "            <author>$1 $2</author>" if (/^([^|]+) \| (.*)$/ && $authors == 1);
+if (/^-+$/ && $authors == 1) {
+        $authors = -1;
+        print "        </authors>";
+}
+
+END {
+    print "    </paper>";
+}
